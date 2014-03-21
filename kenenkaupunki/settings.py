14f1495e8cf5354e41be573b5e8b778c10e0060e
Timo Tuominen <tituomin@gmail.com>
2014-03-21 23:08:00 +0000

    App base, settings.

--- kenenkaupunki/settings.py
+++ kenenkaupunki/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'munigeo',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,10 +60,16 @@ WSGI_APPLICATION = 'kenenkaupunki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'kenenkaupunki',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'vagrant',
+        'PASSWORD': 'vagrant',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
