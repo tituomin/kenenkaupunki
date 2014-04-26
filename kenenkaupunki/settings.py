025348828303699868fb1044e039e12c420b173d
Timo Tuominen <tituomin@gmail.com>
2014-04-26 08:47:26 +0000

    Modeltranslation setup.

--- kenenkaupunki/settings.py
+++ kenenkaupunki/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'modeltranslation',
     'munigeo',
     'south',
     'geoanswers'
@@ -75,7 +76,15 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+gettext = lambda s: s
+LANGUAGES = (
+    ('fi', gettext('Finnish')),
+    ('sv', gettext('Swedish')),
+    ('en', gettext('English')),
+
+)
+LANGUAGE_CODE = 'fi'
+MODELTRANSLATION_DEFAULT_LANGUAGE = LANGUAGE_CODE
 
 TIME_ZONE = 'UTC'
 
